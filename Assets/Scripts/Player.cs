7993d5d4325b965ef7161201a50787a95ebb608e
norbert_blaszczyk_bigcheese <norbert.blaszczyk@bigcheesestudio.com>
2021-02-09 19:16:03 +0100

    collision detection and forward backward added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,26 +1,42 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, IRadius
 {
 	[SerializeField]
-	private Transform targetTransform;
+	private Target target;
 	[SerializeField]
 	private Text theteText;
 	[SerializeField]
 	private Text leftRightText;
+	[SerializeField]
+	private Text collidedText;
+
+	[SerializeField]
+	private float radius;
+
+	public float Radius => radius;
 
 	private void Update()
 	{
 		var vecForward = transform.forward;
-		var vecToEnemy = targetTransform.position - transform.position;
+		var vecToEnemy = target.transform.position - transform.position;
 
-		theteText.text = (Mathf.Acos(Vector3.Dot(vecForward.normalized, vecToEnemy.normalized)) * Mathf.Rad2Deg).ToString();
+		theteText.text = (Mathf.Acos(Vector3.Dot(vecForward.normalized, vecToEnemy.normalized))).ToString();
 
 		var vecRight = transform.right;
 
-		leftRightText.text = (Mathf.Acos(Vector3.Dot(vecRight.normalized, vecToEnemy.normalized)) * Mathf.Rad2Deg) <= 90 ? "right" : "left";
+		string text = (Mathf.Acos(Vector3.Dot(vecRight.normalized, vecToEnemy.normalized))) <= Mathf.PI / 2 ? "right " : "left ";
 
+		text += (Mathf.Acos(Vector3.Dot(vecForward.normalized, vecToEnemy.normalized))) <= Mathf.PI / 2 ? "forward" : "back";
 
+		leftRightText.text = text;
+
+		collidedText.text = (Radius + target.Radius) > Vector3.Distance(transform.position, target.transform.position) ? "collision" : "no collision";
 	}
+}
+
+public interface IRadius
+{
+	float Radius { get; }
 }
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour, IRadius
+{
+	[SerializeField]
+	private float radius;
+
+	public float Radius => radius;
+}

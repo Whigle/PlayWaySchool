711807badc5db9a51e648e0c9dd2ea60d8afedd9
norbert_blaszczyk_bigcheese <norbert.blaszczyk@bigcheesestudio.com>
2021-02-09 18:46:27 +0100

    InitialState

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField]
+	private Transform targetTransform;
+	[SerializeField]
+	private Text theteText;
+
+	private void Update()
+	{
+		var vecForward = transform.forward;
+		var vecToEnemy = targetTransform.position - transform.position;
+
+		theteText.text = (Mathf.Acos(Vector3.Dot(vecForward.normalized, vecToEnemy.normalized)) * Mathf.Rad2Deg).ToString();
+	}
+}
